585db60f35eee46c823f57d819ffbed174cd620c
Shaldin Mathew <matvejsaldin8@gmail.com>
2026-07-13 18:27:59 +0300

    feat: add proxy.ts file

---
+++ client/src/proxy.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PublicRoutes, Routes } from './shared/config/routes';
+
+export function proxy(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+
+	const refreshToken = request.cookies.get('refresh_token')?.value;
+
+	const isPublicRoute = PublicRoutes.includes(pathname as (typeof PublicRoutes)[number]);
+
+	if (isPublicRoute && refreshToken) {
+		return NextResponse.redirect(new URL(Routes.DEFAULT, request.url));
+	}
+
+	if (!isPublicRoute && !refreshToken) {
+		return NextResponse.redirect(new URL(Routes.LOGIN, request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		'/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)).*)',
+	],
+};
--- client/src/shared/config/routes.ts
+++ client/src/shared/config/routes.ts
@@ -2,5 +2,10 @@ export const Routes = {
 	REGISTER: '/register',
 	LOGIN: '/login',
 
+	DEFAULT: '/tasks',
+
 	TASKS: '/tasks'
-}
+} as const
+
+export const PublicRoutes = [Routes.REGISTER, Routes.LOGIN];
+
